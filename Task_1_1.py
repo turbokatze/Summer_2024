397d9a1d9a5b2d299376eb9c16e927fdecc9ddd6
turbokatze <34826674+turbokatze@users.noreply.github.com>
2024-04-24 15:22:36 +0300

    changelog1

--- Task_1_1.py
+++ Task_1_1.py
@@ -1,1 +1,4 @@
-print("Task_1_1")
+number_1 = int(input("Введите число 1: "))
+number_2 = int(input("Введите число 2: "))
+
+print(f"Сумма двух чисел: ", number_1 + number_2, "\n" "Произведение двух чисел: ", number_1 * number_2)
