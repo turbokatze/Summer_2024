fa170c39e5b201d02e49597e58d22f966ae290fd
turbokatze <34826674+turbokatze@users.noreply.github.com>
2024-05-13 17:02:30 +0300

    changelog1

--- Hometasks/Task_3_2.py
+++ Hometasks/Task_3_2.py
@@ -1,8 +1,5 @@
-numbers = str(input())
+input_string = str(input('Введите целое число \n'))
 res = []
-
-for i, k in enumerate(numbers):
-    res.append(k*(i+1))
-
-res.count([])
-print(res)
+for i, k in enumerate(input_string):
+    res.append(k * (i + 1))
+    print(i, "-", input_string.count(str(i)))
