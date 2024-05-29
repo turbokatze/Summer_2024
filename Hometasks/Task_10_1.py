864597da95d62e51b4d71bc2ee33a655d798ab36
turbokatze <34826674+turbokatze@users.noreply.github.com>
2024-05-29 15:08:57 +0300

    changelog1

---
+++ Hometasks/Task_10_1.py
@@ -0,0 +1,11 @@
+# f = open("test1.txt", "wt", encoding='utf-8')
+# f.write('Мой дядя самых честных правил\n' 'Когда не в шутку занемог\n')
+# f.close()
+
+with open('test1.txt', 'r') as sourcetxt, open('test2.txt', 'w') as reversetxt:
+    for line in reversed(sourcetxt.readlines()):
+        for word in reversed(line.split()):
+            word = word.rstrip()
+            reversetxt.writelines(word)
+            reversetxt.write(' ')
+        reversetxt.write('\n')
