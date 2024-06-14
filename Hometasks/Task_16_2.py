336df956e590d686046c24723fdcb762a002df7c
turbokatze <34826674+turbokatze@users.noreply.github.com>
2024-06-14 17:52:15 +0300

    changelog1

--- Hometasks/Task_16_2.py
+++ Hometasks/Task_16_2.py
@@ -1,4 +1,4 @@
-# import re
-#
-# s = input()
-# print(re.findall(r'(\d{1, s}) (\.\d{1,2})?$', r'0 1 12string 44 20 99 100 2'))
+import re
+
+s = input()
+print(re.findall(r'(\b\d{1,2}\b)', r'0 1 12string 44 20 99 100asd 2'))
