99ccc42b7bed798bdf5a630eb793f747e401d0e5
turbokatze <34826674+turbokatze@users.noreply.github.com>
2024-06-21 17:30:41 +0300

    changelog1

---
+++ Hometasks/Task_19_1.py
@@ -0,0 +1,26 @@
+from itertools import combinations, permutations
+
+s = [10, 50, 100, 200, 500, 1000, 2000, 5000]
+quant = []
+
+for i in range(len(s)):
+    for d in combinations(s,2):
+        quant.append(sum(d))
+    for d in combinations(s,3):
+        quant.append(sum(d))
+    for d in combinations(s,4):
+        quant.append(sum(d))
+    for d in combinations(s,5):
+        quant.append(sum(d))
+    for d in combinations(s,6):
+        quant.append(sum(d))
+    for d in combinations(s,7):
+        quant.append(sum(d))
+    for d in combinations(s,8):
+        quant.append(sum(d))
+
+quant_rd = []
+[quant_rd.append(k) for k in quant if k not in quant_rd]
+print(sorted(quant_rd))
+
+
