151293f495f1a235c91beada879b088694255aec
turbokatze <34826674+turbokatze@users.noreply.github.com>
2024-07-05 16:20:18 +0300

    changelog1

---
+++ Hometasks/Task_25_1.py
@@ -0,0 +1,25 @@
+from PyQt6.QtWidgets import QApplication, QPushButton, QMainWindow
+from PyQt6.QtCore import QSize
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Clicker')
+        self.button = QPushButton("Press")
+
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.the_button_was_clicked)
+        self.setCentralWidget(self.button)
+        self.count = 0
+        self.setFixedSize(QSize(200, 150))
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        print(self.count)
+        self.count += 1
+        self.setWindowTitle(f'Clicked {self.count} times!')
+
+
+app, window = QApplication([]), MainWindow(); window.show(); app.exec()
